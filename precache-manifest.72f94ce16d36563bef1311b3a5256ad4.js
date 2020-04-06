e7be3f1416cb84efc12c2786e5b9a6f9bf998c43
outstandingboy <outstandingboy@kakao.com>
2020-04-06 16:38:40 +0900

    Updates

---
+++ precache-manifest.72f94ce16d36563bef1311b3a5256ad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019493b099b0b503101bd53797ccb3db",
+    "url": "/waybill-gen/index.html"
+  },
+  {
+    "revision": "31522fa93a2f888d30a1",
+    "url": "/waybill-gen/static/css/main.2d4d4c48.chunk.css"
+  },
+  {
+    "revision": "1077d57374ca36d63169",
+    "url": "/waybill-gen/static/js/2.8d619b4c.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/waybill-gen/static/js/2.8d619b4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31522fa93a2f888d30a1",
+    "url": "/waybill-gen/static/js/main.65a4d0bc.chunk.js"
+  },
+  {
+    "revision": "799e46d9312819619cbb",
+    "url": "/waybill-gen/static/js/runtime-main.39d85afa.js"
+  }
+]);
